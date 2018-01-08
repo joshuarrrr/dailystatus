4a908734ba04950ff262e01d7a15e4639d89c673
Josh Romero <josh@joshromero.com>
2018-01-08 15:48:09 -0600

    update redirect url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ client_id = os.environ['SLACK_CLIENT_ID']
 client_secret = os.environ['SLACK_CLIENT_SECRET']
 oauth_scope = os.environ['SLACK_BOT_SCOPE']
 state = ''
-redirect_uri = 'https://89d61141.ngrok.io/finish_auth'
+redirect_uri = 'https://dailystatus.herokuapp.com/finish_auth'
 
 app = Flask(__name__)
 
